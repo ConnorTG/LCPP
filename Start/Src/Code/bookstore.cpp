370d317f4b973bb255bc253ad941120439479951
Jim Drake <jimdrake55x@gmail.com>
2017-10-22 16:07:52 -0500

    Some CIN stuff

--- Start/Src/Code/bookstore.cpp
+++ Start/Src/Code/bookstore.cpp
@@ -1,3 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
 int main(){
-    return 0;
+	cout<<"Enter Some numbers"<<endl;
+	int var1 = 0, var2 = 0;
+	cin >> var1 >> var2;
+	cout << "Sum " << var1+var2 <<endl;
 }
