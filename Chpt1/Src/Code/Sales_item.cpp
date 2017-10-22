4ee19293250fe4ed03f1df7fc15f1c2cb8ba07da
Jim Drake <jimdrake55x@gmail.com>
2017-10-22 16:58:56 -0500

    read chapter 1

---
+++ Chpt1/Src/Code/Sales_item.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include "../Headers/Sales_item.h"
+
+
+		
+
+		Sales_Item::Sales_Item(std::string ISBN)
+		{
+			isbn = ISBN;
+		}
---
+++ Chpt1/Src/Headers/Sales_item.h
@@ -0,0 +1,10 @@
+#include <string>
+
+class Sales_Item
+{
+	public:
+		std::string isbn;
+
+		Sales_Item(std::string ISBN);
+		Sales_Item();
+};
